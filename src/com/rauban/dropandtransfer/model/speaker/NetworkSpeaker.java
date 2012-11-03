7cfce7631b519eaa204146a7d2dad7e63bee9690
Robert Borg <robert.borg@gmail.com>
2012-11-03 12:02:17 +0100

    Added discoveryRemoteDeviceAdded to NetworkSpeaker

--- src/com/rauban/dropandtransfer/model/speaker/NetworkSpeaker.java
+++ src/com/rauban/dropandtransfer/model/speaker/NetworkSpeaker.java
@@ -1,7 +1,10 @@
 package com.rauban.dropandtransfer.model.speaker;
 
+import org.fourthline.cling.model.meta.RemoteDevice;
+
 import com.rauban.speaker_listener_pattern.speaker.Speaker;
 
 public interface NetworkSpeaker extends Speaker {
 	public void discoveryStarted();
+	public void discoveryRemoteDeviceAdded(RemoteDevice rd);
 }
--- src/com/rauban/dropandtransfer/model/speaker/baseImpl/NetworkSpeakerBaseImpl.java
+++ src/com/rauban/dropandtransfer/model/speaker/baseImpl/NetworkSpeakerBaseImpl.java
@@ -1,5 +1,7 @@
 package com.rauban.dropandtransfer.model.speaker.baseImpl;
 
+import org.fourthline.cling.model.meta.RemoteDevice;
+
 import com.rauban.dropandtransfer.model.speaker.NetworkSpeaker;
 import com.rauban.dropandtransfer.view.listener.NetworkListener;
 import com.rauban.speaker_listener_pattern.model.ModelBaseImpl;
@@ -11,6 +13,14 @@ public class NetworkSpeakerBaseImpl extends ModelBaseImpl<NetworkListener> imple
 		for(NetworkListener l : listeners){
 			l.discoveryStarted();
 		}
+	}
+
+	@Override
+	public void discoveryRemoteDeviceAdded(RemoteDevice rd) {
+		for(NetworkListener l : listeners){
+			l.discoveryRemoteDeviceAdded(rd);
+		}
+		
 	}
 
 }

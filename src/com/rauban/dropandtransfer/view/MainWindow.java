5a9e13f87f941cfa5f0d309a067293674a92ffa1
Richard Simko <richard@refinedwiki.com>
2015-04-07 09:51:40 +0200

    Format

--- src/com/rauban/dropandtransfer/view/MainWindow.java
+++ src/com/rauban/dropandtransfer/view/MainWindow.java
@@ -1,34 +1,48 @@
-package com.rauban.dropandtransfer.view.newui;
+package com.rauban.dropandtransfer.view;
 
 import com.rauban.dropandtransfer.controller.NetworkController;
+import com.rauban.dropandtransfer.view.listener.NetworkListener;
+import org.fourthline.cling.model.meta.RemoteDevice;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainWindow extends JFrame
+public class MainWindow extends JFrame implements NetworkListener
 {
     private NetworkController controller;
+
+    private boolean uiInitialized = false;
+
+    private JPanel panel;
 
     public MainWindow(NetworkController controller)
     {
         this.controller = controller;
-        this.initializeUI();
+        controller.addListener(this);
 
-    }
-
-    private void initializeUI()
-    {
-        setTitle("Simple example");
+        setTitle("FileDropper");
         setSize(300, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
+        panel = new JPanel();
+        JLabel loadingLabel = new JLabel("Loading...");
+        panel.setLayout(new GridBagLayout());
+        panel.add(loadingLabel);
+        this.add(panel);
+
+        //It's probably OK to always start this
+        controller.start();
+    }
+
+    private synchronized void initializeUI()
+    {
+        if (uiInitialized)
+            return;
+        panel.removeAll();
         panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         panel.setLayout(new GridLayout(5, 5, 5, 5));
-
         JButton searchButton = new JButton("Search");
         searchButton.addActionListener(new ActionListener()
         {
@@ -39,17 +53,6 @@ public class MainWindow extends JFrame
             }
         });
         panel.add(searchButton);
-
-        JButton startButton = new JButton("Start");
-        startButton.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                controller.start();
-            }
-        });
-        panel.add(startButton);
 
         JButton stopButton = new JButton("Stop");
         stopButton.addActionListener(new ActionListener()
@@ -62,9 +65,18 @@ public class MainWindow extends JFrame
         });
         panel.add(stopButton);
 
-
-        this.add(panel);
+        this.uiInitialized = true;
     }
 
+    @Override
+    public void discoveryStarted()
+    {
+        initializeUI();
+    }
 
+    @Override
+    public void discoveryRemoteDeviceAdded(RemoteDevice rd)
+    {
+        System.out.println("Discovered " + rd.getDisplayString());
+    }
 }
